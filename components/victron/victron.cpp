eea78e39a619756133525835f25d34349fd327f2
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-09-17 00:31:32 +0200

    Use the original handle value for the sake of refactoring string allocation.

--- components/victron/victron.cpp
+++ components/victron/victron.cpp
@@ -140,7 +140,7 @@ void VictronComponent::loop() {
       }
       if (c == '\r' || c == '\n') {
         if (this->publishing_) {
-          handle_value_hash_();
+          handle_value_();
         }
         state_ = 0;
       } else {
